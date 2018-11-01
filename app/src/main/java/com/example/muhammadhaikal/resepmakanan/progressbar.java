e3d098a1ecd4af97e5838f4a10183331ffed529b
hahai_45 <haikalhahai356@gmail.com>
2018-11-01 14:59:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/muhammadhaikal/resepmakanan/progressbar.java
@@ -0,0 +1,28 @@
+package com.example.muhammadhaikal.resepmakanan;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class progressbar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progressbar);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(progressbar.this,MainActivity.class);
+                startActivity(i);
+                finish();
+
+            }
+        },3000L);
+    }
+
+
+}
